62a10078ccbe16bc58bac8839e0c98dbfbedb162
ClumsySp <163507178+ClumsySp@users.noreply.github.com>
2025-08-09 00:51:19 +0300

    Step 0: Working optional history

--- impls/cpp/step0_repl.cpp
+++ impls/cpp/step0_repl.cpp
@@ -1,19 +1,14 @@
+#include "linenoise.hpp"
 #include <iostream>
 #include <string>
 
-std::string READ(std::string& input) {
-  return input;
-}
+std::string READ(std::string &input) { return input; }
 
-std::string EVAL(std::string& input) {
-  return input;
-}
+std::string EVAL(std::string &input) { return input; }
 
-std::string PRINT(std::string& input) {
-  return input;
-}
+std::string PRINT(std::string &input) { return input; }
 
-std::string rep(std::string& input) {
+std::string rep(std::string &input) {
   std::string read = READ(input);
   std::string eval = EVAL(read);
   return PRINT(eval);
@@ -21,12 +16,16 @@ std::string rep(std::string& input) {
 
 int main() {
   std::string input;
+  const auto path = "history.txt";
+  linenoise::LoadHistory(path);
   while (1) {
-    std::cout << "user> ";
-    if (!std::getline(std::cin, input)) {
+    auto quit = linenoise::Readline("user> ", input);
+    if (quit) {
       break;
     }
-    std::cout << input << "\n";
+    std::cout << rep(input) << std::endl;
+    linenoise::AddHistory(input.c_str());
   }
+  linenoise::SaveHistory(path);
   return 0;
 }
